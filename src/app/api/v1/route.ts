5dd618329e8d57f732a46b429d8517a5239571a2
Nguyen Thanh Quynh Linh <nguyenthanhquynhlinh@gmail.com>
2024-06-10 21:47:28 +0700

    Done Fix

--- src/app/api/v1/route.ts
+++
@@ -1,8 +0,0 @@
-import { getNewLogin } from "@/controllers/libs/google-auth";
-import { NextResponse } from "next/server";
-
-export async function GET(res: Request) {
-    const data = await getNewLogin();
-    const parseUrl = data.config.url?.replace(/\s/g,"");
-    return NextResponse.json(parseUrl)
-}
